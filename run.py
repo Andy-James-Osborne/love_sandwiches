7fa5ab4c8c4a6f19640bb8906e7547158f21de5a
Andy Osborne <andyjamesosborne@proton.me>
2024-02-15 15:19:29 +0000

    selection 5 rows of data

--- run.py
+++ run.py
@@ -105,6 +105,18 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
     
+def get_last_5_entries_sales():
+    """
+    Collects 5 collumns of data from worksheet
+    total is divided by 5
+    add 10%
+    """
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
 
 def main():
 
